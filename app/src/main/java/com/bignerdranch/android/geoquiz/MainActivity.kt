7f81fd243f1cac9d710682efa34cf4b379960e38
Stephen Dimmick <stephen.dimmick@gmail.com>
2023-05-29 10:36:10 -0400

    Chapter 2 challenges

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.bignerdranch.android.geoquiz
 
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -10,7 +10,8 @@ import androidx.activity.ComponentActivity
 class MainActivity : ComponentActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var prevButton: ImageButton
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -31,7 +32,12 @@ class MainActivity : ComponentActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        prevButton = findViewById(R.id.prev_button)
         questionTextView = findViewById(R.id.question_text_view)
+
+        questionTextView.setOnClickListener {
+            nextQuestion()
+        }
 
         trueButton.setOnClickListener {
             checkAnswer(true)
@@ -41,11 +47,20 @@ class MainActivity : ComponentActivity() {
             checkAnswer(false)
         }
 
-        nextButton.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
+        prevButton.setOnClickListener {
+            currentIndex = (currentIndex - 1 + questionBank.size) % questionBank.size
             updateQuestion()
         }
 
+        nextButton.setOnClickListener {
+            nextQuestion()
+        }
+
+        updateQuestion()
+    }
+
+    private fun nextQuestion() {
+        currentIndex = (currentIndex + 1) % questionBank.size
         updateQuestion()
     }
 
